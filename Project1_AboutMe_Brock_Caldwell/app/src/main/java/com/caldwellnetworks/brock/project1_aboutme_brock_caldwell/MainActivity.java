fafd73ba2e61d88a92511f2d0a3a4b6dc0a8280d
BrockCaldwell <brockcaldwell33@gmail.com>
2017-01-21 13:45:02 -0600

    Added Project_1_About_Me

---
+++ Project1_AboutMe_Brock_Caldwell/app/src/main/java/com/caldwellnetworks/brock/project1_aboutme_brock_caldwell/MainActivity.java
@@ -0,0 +1,13 @@
+package com.caldwellnetworks.brock.project1_aboutme_brock_caldwell;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
